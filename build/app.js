6481ec2958008499f2fbc1ef34a6390397344384
Brian Shef <brian.shef@rewardstyle.com>
2016-07-26 11:00:47 -0500

    Comment out unused boilerplate and add instructions

--- build/app.js
+++ build/app.js
@@ -6,10 +6,6 @@ var os = _interopDefault(require('os'));
 var electron = require('electron');
 var jetpack = _interopDefault(require('fs-jetpack'));
 
-var greet = function () {
-    return 'Hello World!';
-};
-
 // The variables have been written to `env.json` by the build process.
 var env = jetpack.cwd(__dirname).read('env.json', 'json');
 
@@ -18,14 +14,13 @@ console.log('Loaded environment variables:', env);
 var app = electron.remote.app;
 var appDir = jetpack.cwd(app.getAppPath());
 
-// Holy crap! This is browser window with HTML and stuff, but I can read
-// here files like it is node.js! Welcome to Electron world :)
 console.log('The author of this app is:', appDir.read('package.json', 'json').author);
+console.info('PLATFORM:', os.platform(), 'ENVIRONMENT:', env.name);
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = env.name;
+    // document.getElementById('greet').innerHTML = greet();
+    // document.getElementById('platform-info').innerHTML = os.platform();
+    // document.getElementById('env-name').innerHTML = env.name;    
 });
 }());
 //# sourceMappingURL=app.js.map
