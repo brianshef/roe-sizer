412cfda9b5065df9ba8650107cdd402f755d45be
Brian Shef <brian.shef@rewardstyle.com>
2016-07-27 09:25:22 -0500

    Add debugging information and status message update logic

--- build/app.js
+++ build/app.js
@@ -17,10 +17,39 @@ var appDir = jetpack.cwd(app.getAppPath());
 console.log('The author of this app is:', appDir.read('package.json', 'json').author);
 console.info('PLATFORM:', os.platform(), 'ENVIRONMENT:', env.name);
 
+//  Debug variables
+var pkgName     = appDir.read('package.json', 'json').name;
+var pkgVersion  = appDir.read('package.json', 'json').version;
+var pkgAuthor   = appDir.read('package.json', 'json').author;
+var platform    = os.platform().toUpperCase();
+var environment = env.name.toUpperCase();
+var debugString = pkgName + ' ' + pkgVersion + ' by ' + pkgAuthor + ' @ ' + platform + ' ' + environment;
+
+var statusId      = 'status';
+
+//  Sets the inner HTML of the element with the specified ID to the specified
+//  content, if the element exists.
+function _setContent (id, content, callback) {
+  var element = document.getElementById(id);
+  if (element && element.innerHTML) {
+    element.innerHTML = content;
+    console.info('Set', id, 'content to', content);
+  } else {
+    console.warning('Invalid element ID', id);
+  }
+
+  if (callback) {
+    callback();
+  }
+}
+
+//  Updates the status message to the specified message content.
+function updateStatus (msg) {
+  _setContent(statusId, msg);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-    // document.getElementById('greet').innerHTML = greet();
-    // document.getElementById('platform-info').innerHTML = os.platform();
-    // document.getElementById('env-name').innerHTML = env.name;    
+    updateStatus(debugString);
 });
 }());
 //# sourceMappingURL=app.js.map
