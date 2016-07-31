6b8aa400a97775a9c787abd3ed2ba5ea120c1d1c
Brian Shef <brian.shef@rewardstyle.com>
2016-07-30 21:12:12 -0500

    BROKEN - Refactor image processing logic further for maintainability

--- build/background.js
+++ build/background.js
@@ -121,12 +121,38 @@ var fs    = require('fs');
 var path  = require('path');
 var im    = require('imagemagick');
 
+
+var processImages = function(data, callback) {
+  _validateImageProcessingData(data, function(data) {
+    var inputDir  = data['inputDir'];
+    var outputDir = data['outputDir'];
+    var width     = data['width'];
+    var queue     = [];
+
+    var files = fs.readdirSync(inputDir);
+    _enqueueValidImages(files, queue, inputDir, outputDir, width, function(queue) {
+      _performProcessing(queue, function(response) {
+        if (callback) { callback(response); }
+      });
+    });
+  });
+}
+
+function _validateImageProcessingData (data, callback) {
+  var isValid = data && data.outputDir != '' && data.inputDir != '' && data.width > 0;
+  if (isValid && callback) {
+    callback(data);
+  } else {
+    console.warn('Invalid image processing data', data);
+  }
+}
+
 //  Enqueue only valid images and then process the queue with a callback
 function _enqueueValidImages(files, queue, inputDir, outputDir, width, callback) {
   var allowedExtensions = ['.JPG'];
   files.forEach(function(file, index) {
     var imgSrc = path.join(inputDir, file);
-    var imgDst = path.join(outputDir, 'resized_' + file);
+    var imgDst = path.join(outputDir, 'resized_' + width + '_' + file);
     var ext    = path.extname(file).toUpperCase();
 
     if (allowedExtensions.indexOf(ext) > -1) {
@@ -143,51 +169,30 @@ function _enqueueValidImages(files, queue, inputDir, outputDir, width, callback)
 
 //  Perform the image processing on the queue of image data
 function _performProcessing(queue, callback) {
-  console.log('[DEBUG processImages()]', queue);
   queue.forEach(function(img, index) {
     var src   = img['src'];
     var dst   = img['dst'];
     var width = img['width'];
 
     console.log('(', index + 1, 'of', queue.length, ')', 'Processing', src,  '...');
-
-    im.resize({
-      srcPath: src,
-      dstPath: dst,
-      width:   width
-    }, function(err, stdout, stderr) {
-      if (err) { throw err };
-      console.log('Resized', src, 'to width', width, 'and output as', dst);
-      if (callback) { callback('DONE'); }
-    });
+    _resizeImage(src, dst, width);
   });
+
+  if (callback) { callback('DONE'); }
 }
 
-function _validateImageProcessingData (data, callback) {
-  var isValid = data && data.outputDir != '' && data.inputDir != '' && data.width > 0;
-  if (isValid && callback) {
-    callback(data);
-  } else {
-    console.warn('Invalid image processing data', data);
-  }
-}
-
-var processImages = function(data, callback) {
-  _validateImageProcessingData(data, function(data) {
-    var inputDir  = data['inputDir'];
-    var outputDir = data['outputDir'];
-    var width     = data['width'];
-    var queue     = [];
-
-    fs.readdir(inputDir, function(err, files) {
-      if (err) { throw err; }
-      _enqueueValidImages(files, queue, inputDir, outputDir, width, function(queue) {
-        _performProcessing(queue, function(response) {
-          if (callback) { callback(response); }
-        });
-      });
-    });
+//  Resizes a src image and outputs to dst
+function _resizeImage(src, dst, width, callback) {
+  im.resize({
+    srcPath: src,
+    dstPath: dst,
+    width:   width
+  }, function(err, stdout, stderr) {
+    if (err) { throw err };
+    console.log('Resized', src, 'to width', width, 'and output as', dst);
   });
+
+  if (callback) { callback(); }
 }
 
 // The variables have been written to `env.json` by the build process.
